66a41db9c66ea996ed5a6ca9229299acc3edb3f8
轻编程 <2539909370@qq.com>
2021-10-18 15:50:16 +0800

    第十三章  博客的分页功能

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q, F
+from django.core.paginator import Paginator
 # Create your views here.
 from .models import Category, Post
 
 
 def index(request):
     # 首页
-    post_list = Post.objects.all()   # 查询到所有的文章
-    context = {'post_list': post_list }
+    post_list = Post.objects.all()   # 查询到所有的文章,queryset
+    # 分页方法
+    paginator = Paginator(post_list, 2)  # 第二个参数2代表每页显示几个
+    page_number = request.GET.get('page')   # http://assas.co/?page=1 (页码)
+    page_obj = paginator.get_page(page_number)
+    context = {'page_obj': page_obj}
     return render(request, 'blog/index.html', context)
 
 
@@ -15,7 +20,10 @@ def category_list(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     # 获取当前分类下的所有文章
     posts = category.post_set.all()
-    context = {'category': category, 'post_list': posts}
+    paginator = Paginator(posts, 2)  # 第二个参数2代表每页显示几个
+    page_number = request.GET.get('page')   # http://assas.co/?page=1 (页码)
+    page_obj = paginator.get_page(page_number)
+    context = {'category': category, 'page_obj': page_obj }
     return render(request, 'blog/list.html', context)
 
 
@@ -45,8 +53,11 @@ def search(request):
     else:
         # 包含查询的方法，用Q对象来组合复杂查询，title__icontains 他两个之间用的是双下划线（__）链接
         post_list = Post.objects.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword) | Q(content__icontains=keyword))
+    paginator = Paginator(post_list, 2)  # 第二个参数2代表每页显示几个
+    page_number = request.GET.get('page')   # http://assas.co/?page=1 (页码)
+    page_obj = paginator.get_page(page_number)
     context = {
-        'post_list': post_list
+        'page_obj': page_obj
     }
     return render(request, 'blog/index.html', context )
 
